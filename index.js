4fdc36413258a4c21371b06549ac03a7a5842aed
Δημήτρης Ραβιόλος <theio.vrefos@gmail.com>
2016-02-10 21:27:56 +0200

    Init function

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 var fs = require('fs')
-var m  = require('mustache')
+var m = require('mustache')
+
+var _json = (dir, json) => {fs.writeFileSync(dir + 'dictum.json', JSON.stringify(json))}
+
+function init(dir) {
+	var content = fs.readdirSync(dir)
+
+	try {
+		var json = JSON.parse(fs.readFileSync(dir + 'dictum.json'))
+	} catch(err) {
+		var json = {}
+	}
+
+	for(var i = 0; i < content.length; i++) {
+		var stat = fs.statSync(dir + content[i])
+		var name = content[i]
+
+		if(stat && stat.isDirectory()) {
+			init(dir + content[i] + '/')
+		}
+
+		json.name = ''
+	}
+
+	fs.writeFileSync(dir + 'dictum.json', JSON.stringify(json))
+}
+
+function render(dir) {
+	var content = fs.readdirSync(dir)
+	var html = ""
+
+	try {
+		var json = JSON.parse(fs.readFileSync(dir + 'dictum.json'))
+	} catch(err) {
+		var json = {}
+	}
+
+	for(var i = 0; i < content.length; i++) {
+		var stat = fs.statSync(dir + content[i])
+		var name = content[i]
+
+		if(stat && stat.isDirectory()) {
+			html += render(dir + content[i] + '/')
+		} else {
+			html += fs.readFileSync(dir + name).toString()
+		}
+	}
+
+	return html
+}
 
 function parse(dir) {
 	var data = JSON.parse(fs.readFileSync(dir + 'dictum.json'))
